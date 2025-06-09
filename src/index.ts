5cdcaeb19a06251535b028f0be9c3430066e6c48
Orchide Irakoze Sr <orchide@Orchides-MacBook-Pro.local>
2025-06-09 16:30:10 +0200

    added global dotenv

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import updateNotifier from 'update-notifier';
 import pkg from '../package.json';
 import chalk from 'chalk';
 import { registerLoginCommand } from './commands/login';
-import { register } from 'module';
 import { registerDiagnoseCommand } from './commands/diagnose';
-
+import 'dotenv/config';
 const program = new Command();
 
 // Optional: check for updates
